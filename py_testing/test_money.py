9465ce27c9b97cdcf406a8547cff78c9fae0d7f5
leonardtseng <leonard.sh.tseng@foxconn.com>
2021-12-26 12:49:18 +0800

    refactory: moved Money and Portfolio classes their own Python files

--- py_testing/test_money.py
+++ py_testing/test_money.py
@@ -2,6 +2,8 @@ import unittest
 import operator
 import functools
 
+from money import Money
+from portfolio import Portfolio
 """
 1. Importing the unittest package, needed for the TestCase superclass '
 2. Our test class, which must subclass the unittest.TestCase class 
@@ -10,7 +12,7 @@ import functools
 5. Method under test: times — which also does not exist yet
 6. Comparing actual value with expected value in a assertEqual statement
 
-python py_testing/test_money.py
+python py_testing/test_money.py                
 """
 
 # class Dollar:
@@ -20,44 +22,11 @@ python py_testing/test_money.py
 #     def times(self, multiplier):
 #         return Dollar(self.amount * multiplier)
 
-
-class Money:
-    def __init__(self, amount, currency):
-        self.amount = amount
-        self.currency = currency
-    
-    def times(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-    
-    def divide(self, divisor):
-        return Money(self.amount / divisor, self.currency)
-
-    def __eq__(self, other):
-        return self.amount == other.amount and self.currency == other.currency
-
-
-class Portfolio:
-    def __init__(self): 
-        self.moneys = []
-
-    def add(self, *moneys):
-        self.moneys.extend(moneys)
-    
-    def evaluate(self, currency): 
-        total = functools.reduce(operator.add, map(lambda m: m.amount, self.moneys), 0) 
-        return Money(total, currency)
-
-
 class TestMoney(unittest.TestCase): 
-    def testMultiplicationInDollars(self):
+    def testMultiplication(self):
         fiveDollars = Money(5, "USD")
         tenDollars = Money(10, "USD") 
         self.assertEqual(tenDollars, fiveDollars.times(2))
-    
-    def testMultiplicationInEuros(self):
-        tenEuros = Money(10, "EUR")
-        twentyEuros = Money(20, "EUR") 
-        self.assertEqual(twentyEuros, tenEuros.times(2))
     
     def testDivision(self):
         originalMoney = Money(4002, "KRW") 
